dedc632a93f52dfdbb212dd789ecd8908abb0909
Darrell King <kingdarrella@gmail.com>
2018-09-21 07:41:10 +0100

    Created platform/springboot2 as a clone of platform/springboot

---
+++ platforms/springboot2/src/main/java/io/hawt/springboot2/HawtioProperties.java
@@ -0,0 +1,21 @@
+package io.hawt.springboot2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Configuration properties for hawtio
+ */
+public class HawtioProperties {
+
+    private Map<String, String> properties = new HashMap<>();
+
+    public HawtioProperties(final Map<String, String> properties) {
+        this.properties = Objects.requireNonNull(properties);
+    }
+
+    public Map<String, String> get() {
+        return properties;
+    }
+}
